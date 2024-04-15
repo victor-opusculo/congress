b2c2399fb6db112996f9fd3fca83c2ba053ba98e
Victor Opusculo <victor.ventura@uol.com.br>
2024-04-15 18:55:52 -0300

    Slides de programação adicionados

--- components/Site/ProgSlideShow.php
+++ components/Site/ProgSlideShow.php
@@ -27,8 +27,8 @@ class ProgSlideShow extends Component
                 range(1, count($this->slidesFiles)) 
             ),
 
-            View::tag('button', type: 'button', class: 'prevSlideButton text-[5rem] absolute flex items-end ml-4 cursor-pointer hover:text-neutral-300 left-0 top-0 bottom-0 text-white font-bold', children: [ View::text('⇦') ]),
-            View::tag('button', type: 'button', class: 'nextSlideButton text-[5rem] absolute flex items-end mr-4 cursor-pointer hover:text-neutral-300 right-0 top-0 bottom-0 text-white font-bold -scale-x-100', children: [ View::text('⇦') ]),
+            View::tag('button', type: 'button', class: 'prevSlideButton text-[3rem] absolute flex items-end ml-4 cursor-pointer hover:text-neutral-300 left-0 top-0 bottom-0 text-white font-bold', children: [ View::text('⇦') ]),
+            View::tag('button', type: 'button', class: 'nextSlideButton text-[3rem] absolute flex items-end mr-4 cursor-pointer hover:text-neutral-300 right-0 top-0 bottom-0 text-white font-bold -scale-x-100', children: [ View::text('⇦') ]),
         ]);
     }
 }
